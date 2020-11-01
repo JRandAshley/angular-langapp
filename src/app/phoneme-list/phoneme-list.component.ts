a8547d7fb7743fd071691dc36eb5b3ca04d05cdb
YoYoDrif <randashley@rocketmail.com>
2020-11-01 17:21:40 -0500

    1 file modified, 3 added

---
+++ src/app/phoneme-list/phoneme-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-phoneme-list',
+  templateUrl: './phoneme-list.component.html',
+  styleUrls: ['./phoneme-list.component.css']
+})
+export class PhonemeListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
